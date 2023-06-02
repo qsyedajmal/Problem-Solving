2a72062ae7b1fb6f864cff3fadc720d3a9d3507d
syedajmalq <syedajmalq@gmail.com>
2023-06-02 16:35:21 +0530

    1-Problem

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+//Write a program to find the sum of two numbers.
+class Sum{
+     static public void sum(int i,int j){
+        int s=i+j;
+        System.out.println(s);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        //Sum.sum(5,10);
+        //LargestNum.largestNum(50,100,1000);
+        //Prime.prime(5);
+        //Palindrome.palindrome("Madam");
+        //System.out.println(Factorial.factorial(5));
+        //Fibonacci.fibonacci(10);//0 1 1 2 3 5 8 13 21 34
+        //ResverseString.reverseString("Ajmal");
+        //int array[]= {5,4,3,2,1};
+        //int[] array2={2,5};
+        //ArrayAscendindOrder.arrayAscendingOrder(array);
+        //String[] arr1 = { "Article", "in", "Geeks", "for", "Geeks" };
+        //String[] arr2 = { "Geeks", "for", "Geeks" };
+        //CommonElements.commonElements(array,array2);
+        //String name ="Q Syed Ajmal";
+        //OccurrenceOfCharacter.occurrenceOfCharacter(name);
+
+
+
+    }
+}
